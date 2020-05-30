494ec3467e2b53594fb0292c2268b25f17745ae7
AVasilyev1998 <avasilyev1998@mail.ru>
2020-05-30 22:04:37 +0300

    heroku reqs updated

--- LinkShortener/urls.py
+++ LinkShortener/urls.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
+
+
+def main_redirect(request):
+    return redirect('shortener:index')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shortener/', include('shortener.urls')),
+    path('shortener/', include('shortener.urls'), name='shortener'),
+    path('', main_redirect),
 ]
+
+
--- shortener/urls.py
+++ shortener/urls.py
@@ -4,7 +4,6 @@ from .views import index, link_redirector
 app_name = 'shortener'
 
 urlpatterns = [
-    # path('links/', ApiLinksView.as_view(), name='view'),
     path('', index, name='index'),
     re_path(r'(?P<short_url>\w{0,120})/$', link_redirector, name='redirector'),
 ]
